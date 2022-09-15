e8eb7b16da7a8ef528e10214a31a7d98d800718a
Alex Jhones <alexjhones286@gmail.com>
2022-09-14 21:25:18 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>upImagens</title>
+</head>
+<body>
+
+    <form enctype="multipart/form-data" action="">
+        <p><label for="">Selecione o arquivo</label>
+        <input type="file"></p>
+        <button type="submit">Enviar arquivo</button>
+    </form>
+    
+</body>
+</html>
