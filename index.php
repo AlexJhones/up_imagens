49debb1b7ba3ffb0880c7ce1f71e5b3ca1a4e6b1
Alex Jhones <alexjhones286@gmail.com>
2022-09-15 21:26:44 -0300

    css inicial

--- index.php
+++ index.php
@@ -38,34 +38,54 @@ $sql_query = $mysqli->query("SELECT * FROM arquivos") or die($mysqli->error);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>upImagens</title>
+    <link rel="stylesheet" href="./styles.css">
+    <!-- FONTS GOOGLE -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
+    <title>upImagens</title> <!-- New title: Vision -->
 </head>
 <body>
+<div class="container">
 
-    <form method="POST" enctype="multipart/form-data" action="">
-        <p><label for="">Selecione o arquivo</label>
-        <input name="arquivo" type="file"></p>
-        <button name="upload" type="submit">Enviar arquivo</button>
-    </form>
+    <header>
+        <h1>Envie e compartilhe suas imagens de forma simples e direta.</h1>
+        <h2>Crie álbuns e compartilhe de modo público ou privado, protegido por senha ou qualquer pessoa que tenha o link.</h2>
+    </header>
 
-    <h1>Lista de Arquivos</h1>
-    <table border="1" cellpadding="10">
-        <thead>
-            <th>Preview</th>
-            <th>Arquivo</th>
-            <th>Data de Envio</th>
-        </thead>
+    <nav>
 
-        <tbody>
-            <?php while($arquivo = $sql_query->fetch_assoc()) { ?>
-            <tr>
-                <th><img height="50" src="<?php echo $arquivo['path'] ?>" alt=""></th>
-                <td><a target="_blank" href="<?php echo $arquivo['path'] ?>"><?php echo $arquivo['name_arquivo']; ?></a></td>
-                <td><?php echo date("d/m/Y H:i", strtotime($arquivo['data_upload'])); ?></td>
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-    
+    </nav>
+
+    <main>
+        <form method="POST" enctype="multipart/form-data" action="">
+            <!-- <p><label for="">Selecione o arquivo</label></p> -->
+            <input name="arquivo" type="file"><br>
+            <button name="upload" type="submit">Enviar Arquivos</button>
+        </form>
+
+        <h2>Lista de Arquivos</h2>
+        <table border="1" cellpadding="10">
+            <thead>
+                <th>Visualizar</th>
+                <th>Baixar arquivo</th>
+                <th>Data do envio</th>
+            </thead>
+
+            <tbody>
+                <?php while($arquivo = $sql_query->fetch_assoc()) { ?>
+                <tr>
+                    <th><a target="_blank" href="<?php echo $arquivo['path'] ?>"><img height="50" src="<?php echo $arquivo['path'] ?>" alt=""></a></th>
+                    <td><a href="<?php echo $arquivo['path'] ?>" download="<?php $arquivo['name_arquivo']; ?>"><?php echo $arquivo['name_arquivo']; ?></a></td>
+                    <td><?php echo date("d/m/Y H:i", strtotime($arquivo['data_upload'])); ?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </main>
+
+    <footer>O uso do upImagens constitui a aceitação de nossos Termos de Serviço e Política de Privacidade.</footer>
+
+</div>
 </body>
 </html>
